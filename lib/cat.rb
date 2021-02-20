6da7bd357ee4c13e37a8b177a3c11e9fe0102143
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-20 12:35:13 -0500

    commit tree 119b6f9a932179ab2fa8f9d378b680f2fbb4d747

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  attr_reader :species, :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @species = species
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,78 @@
+require "pry"
+
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :cats
+  attr_reader :name, :species
+
+  @@pets = {:dogs => [], :cats => []}
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+    # binding.pry
+  end
+
+  def self.count
+    Owner.all.count
+  end
+
+  def self.reset_all
+    @@all.clear()
+  end
+
+  def cats
+    Cat.all.select {|c| c.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|d| d.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood = "happy"
+    end
+    
+    def feed_cats
+      Cat.all.each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def pets
+      @@pets
+    end
+
+    def sell_pets
+      new_array = Dog.all.select {|dog| dog.owner == self} + Cat.all.select {|cat| cat.owner == self}
+      new_array.each do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+      end
+      new_array.clear
+      # binding.pry
+    end
+
+    def list_pets
+      "I have #{Dog.all.count {|dog| dog.owner == self}} dog(s), and #{Cat.all.count {|cat| cat.owner == self}} cat(s)."
+    end
+  end
 end
